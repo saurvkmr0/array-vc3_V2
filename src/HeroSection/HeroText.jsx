0185d2a9b846c96b2c56556896c2247eaacd3b48
saurvkmr0 <saurvkmr0@gmail.com>
2024-10-16 23:47:46 +0530

    hero section added

--- src/HeroSection/HeroText.jsx
+++ src/HeroSection/HeroText.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import { BsChevronDown } from "react-icons/bs";
 
 const HeroText = () => {
   return (
     <>
-        <div className="dark:text-white text-customBlack mx-4 lg:mx-40 py-4 md:py-8">
-          <h1 className="text-center text-3xl md:text-6xl lg:text-7xl font-bold">
+        <div className="text-center dark:text-white text-customBlack mx-4 lg:mx-40 py-20 md:py-8 flex flex-col gap-6 md:gap-14">
+          <h1 className="text-3xl md:text-6xl lg:text-7xl font-black">
             We’re called First Round 
             <br/>
             for a reason.
           </h1>
-          {/* <p>Vision Needs Liquidity</p> */}
+          <p className="text-xl md:text-3xl lg:text-4xl font-bold">Vision Needs Liquidity</p>
+          <p className="text-lg font-[500] px-10 lg:px-40">Your first money in is one thing. But so are your first hires, your first product and your first customers. We’ve helped Notion, Roblox, Uber, Square, Verkada, Together and 500 others tackle these firsts and more.</p>
+          <div className="flex justify-center text-2xl md:text-4xl"><BsChevronDown /></div>
         </div>
     </>
   )
